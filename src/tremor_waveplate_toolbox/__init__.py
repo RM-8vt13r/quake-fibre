a8fc092d0f32abda054e396c8177d4b2a32b8fb1
Rick M. Butler <r.m.butler@tue.nl>
2025-05-14 17:27:36 +0200

    Updated __init__.py to reflect naming changes and new classes

--- src/tremor_waveplate_toolbox/__init__.py
+++ src/tremor_waveplate_toolbox/__init__.py
@@ -1,9 +1,11 @@
 """
 Initialise the tremor-waveplate-toolbox module by specifying its properties and importing all its functionality.
 """
-from .waveplate import Waveplate
+from .fibre import Fibre
 from .signal import Signal
 from .transmitter import Transmitter
+from .receiver import Receiver
 from .pulse import Pulse, Sinc, SINC, RootRaisedCosine, RRCOS
 from .constellation import Constellation, BPSK, QPSK, PSK8, QAM4, QAM16, QAM64
 from .constants import Domain, PAULI_1, PAULI_2, PAULI_3, PAULI_VECTOR
+from .earthquake import Earthquake
